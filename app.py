d47c0c98ff328f89f21bcede39648b80f53aab46
Interestingkiwi <jasondruckenmiller@gmail.com>
2025-10-30 12:40:58 -0400

    Fixed lineup scaricity calc

--- app.py
+++ app.py
@@ -295,7 +295,10 @@ def get_optimal_lineup(players, lineup_settings):
 
         slot_scarcity = {}
         for slot in available_slots_for_player:
-            scarcity_count = sum(1 for other in player_pool if other != player and slot in [p.strip() for p in get_pos_str(other).split(',')])
+            scarcity_count = sum(1 for other in player_pool
+                                     if other != player and
+                                     other.get('player_id') not in assigned_player_ids and
+                                     slot in [p.strip() for p in get_pos_str(other).split(',')])
             slot_scarcity[slot] = scarcity_count
 
         best_pos = min(slot_scarcity, key=slot_scarcity.get)
